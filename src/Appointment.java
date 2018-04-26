22d09646cd2ec04860d1fa1671f616027737469f
demetozby <poreneo@hotmail.com>
2018-04-26 23:59:30 +0200

    Date ve Time oluşturuldu

--- src/Appointment.java
+++ src/Appointment.java
@@ -1,9 +1,31 @@
+import java.sql.Date;
 
 public class Appointment extends Patient
 {
 	
-	
-	
-	
+	private Date date; 
+	private String time; 
+	 public String getTime() {
+		return time;
+	}
+
+
+
+	public Appointment(Date date) {
+	        this.date = date;
+	        this.time = time;
+	    }
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public void setTime(String time) {
+		this.time = time;
+		
+	}
 
 }
