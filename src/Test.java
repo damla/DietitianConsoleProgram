f3efe1b630dce9009b78b6ae05ea495ef4b7f2ce
Damla Koksal <damlakoksal@outlook.com>
2018-05-04 20:45:02 +0300

    #8 Started to console part of project.

--- src/Test.java
+++ src/Test.java
@@ -5,23 +5,86 @@ import java.util.Scanner;
 public class Test{
 
 		  public static void main(String[] args) {
+			  //Example patients
+			  LinkedList<Patient> ml = new LinkedList<Patient>();
+			  BinarySearchTree bst = new BinarySearchTree();
+			  
+			  Patient a=new Patient("Damla", "Koksal",51, 1.61);
+			  a.setBirthday("10/10/1996");
+			  a.setPhoneNo("0(212)222-2222");
+			  a.setDateAppointment("10/04/2018");
+			  a.setTimeAppointment("10:30");
+			  a.setdList("Mukemmel,gerek yok.");
+			  ml.add(a); //added to index #0 of linked list.
+			  bst.insert(a.getpId()); //added the id to binary search tree.
+			  
+			  Patient b=new Patient("Demet", "Ozbay", 45, 1.55);
+			  b.setBirthday("02/05/1992");
+			  b.setPhoneNo("0532222-2222");
+			  ml.add(b.getpId()-1,b); //setted b to index #1 of linked list.
+			  bst.insert(b.getpId()); //added the id to binary search tree.
+			  
+			  Patient c=new Patient("Yaprak", "Koksal", 58, 1.61);
+			  ml.add(c.getpId()-1,c);
+			  bst.insert(c.getpId());
+			  
+			/*  for (Patient element : ml)
+			      System.out.println(element + "\n");
+			  
+			  System.out.println("inorder print binary:");
+			  bst.inorder(bst.getRoot());
+			  */
+			  char ch;
+				do
+				   {
+				System.out.print("Welcome to the Patient Customize for Dietitian Program!" +
+								   "\nOur console application is working with numbers so you have to select which way you want to go by numbers."+
+								   "\n *Select (1) to get patient's information."+
+								   "\n *Select (2) to see appoinments."+
+								   "\n *Select (3) to create new patient."+
+								   "\nYour selection: ");
+				String username;
+				Scanner sc=new Scanner(System.in);
+				int choice = sc.nextInt();            
+		        switch (choice)
+		        {
+		       
+		        case 1 : 
+		            System.out.println("Please enter the username of patient: ");
+		            username=sc.nextLine();
+		       
+		            	for(int i=0;i<bst.getRoot().getElement()-1; i++) { //root ve id iliskisini coz.
+		            		if(!ml.get(i).getUsername().equals(username)){ //donguyu onlari kullanarak karsilastir.
+		            			username="Not valid.";
+		            		}
+		            	}
+		       System.out.println(username);
+		            break;
+		        
+		        case 2 : 
+		            System.out.println("2 yi secti");
+		            
+		            break;                         
+		        
+		        case 3 : 
+		            System.out.println("3 u secti");
+		        
+		            break;                                          
+		        
+		        default : 
+		            System.out.println("Please select an option from the list belove. \n ");
+		            System.out.println("\nDo you want to continue (Type y or n) \n");
+		            break;
+		        }    
+		        ch = sc.next().charAt(0);
+		    } while (ch == 'Y'|| ch == 'y');   
 			
-			  Patient d=new Patient("Damla", "Koksal",51, 1.61);
-			  d.setBirthday("10/10/1996");
-			  d.setBmi();
-			  d.setUsername();
-			  d.setPhoneNo("0(212)222-2222");
-			  d.setDateAppointment("10/04/2018");
-			  d.setTimeAppointment("10:30");
-			  d.setdList("Mukemmel,gerek yok.");
-			  d.setBmi(); //!! dikkat height weight set edildikten sonra yap
+		
 			  
-			  System.out.println(d.toString());
-			  System.out.println(); 
 			  
-			  Scanner s = new Scanner(System.in);
-			  int x;
-			  BinarySearchTree b = new BinarySearchTree();
+			  
+			  
+			/*
 			  b.insert(d.getpId());
 			  b.insert(2);
 			  b.insert(3);
@@ -39,14 +102,14 @@ public class Test{
 			  b.inorder(b.getRoot());
 			  
 			  System.out.println();
-			  LinkedList<Patient> ml = new LinkedList<Patient>();
+			 
 
 			   ml.add(d);
 			   ml.add(1, new Patient("Demet","Ozbay",45,1.55));
 			   if(!b.search(d.getpId()))
 				  ml.remove(d.getpId()-1);
 			    for (Patient element : ml)
-			      System.out.println(element + "\n");
+			      System.out.println(element + "\n");	  */
 			/*  System.out.println("Enter a number to delete from the tree: ");
 			  x = s.nextInt();
 			  if(b.delete(x))
@@ -74,7 +137,6 @@ public class Test{
 		
 	/* 3.TEST System.out.println("Linked Listimiz:");
 			
-		
 		
 		
 		
